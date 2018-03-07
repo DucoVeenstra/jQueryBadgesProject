ace353d1bc9de8d9c41f87e2941efc1eee713ab9
Duco Veenstra <d.veenstra@ballast-nedam.nl>
2018-03-07 14:45:58 +0100

    change code from .map

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,22 +1,34 @@
 $(function() {
-
-  // your code will go here
-  var $badges = $('#badges');
-
   $.ajax({
     url: 'https://www.codeschool.com/users/DV84.json',
     dataType: 'jsonp',
-    success: function(response) {
-      // handle response
-      console.log(response);
-      
-      response.badges.map((badge) => {
-        $div = $(`<div class="course" />`).appendTo($badges);
-
-        $(`<h3>${badge.name}</h3>`).appendTo($div);
-        $(`<img src="${badge.badge}"/>`).appendTo($div);
-        $(`${badge.course_url ? "<a class='btn btn-primary' href=" + badge.course_url + " target='_blank'>See Course</a>" : ""}`).appendTo($div);
+    success: function(data) {
+      console.log(data);
+      populateCourses(data.courses.completed)
+    }
+  });
+  
+  function populateCourses(courses) {
+    var $badges = $('#badges');
+    courses.forEach(function(course) {
+        $div = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges);
+  
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($div);
+  
+        $('<img />', {
+          src: course.badge
+        }).appendTo($div);
+  
+        $('<a />', {
+          'class': 'btn btn-primary',
+          target: '_blank',
+          href: course.url,
+          text: 'See Course'
+        }).appendTo($div);
       });
     }
   });
-});
